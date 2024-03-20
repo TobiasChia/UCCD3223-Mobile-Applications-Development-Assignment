8e6d6e368b2355c8f953cecf62addd2ac1e8dc76
TobiasChia <tobiaschia322@gmail.com>
2024-03-20 13:29:23 +0800

    Math

--- MathAPP2/app/src/main/java/com/example/mathapp/CompareNumberActivity.java
+++ MathAPP2/app/src/main/java/com/example/mathapp/CompareNumberActivity.java
@@ -5,15 +5,16 @@ import androidx.core.content.ContextCompat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import java.util.Random;
 import android.content.Intent;
-
 
 public class CompareNumberActivity extends AppCompatActivity {
     private TextView number1, number2;
     private final Random random = new Random();
     private final Handler handler = new Handler();
+    private boolean isGreaterThanRule = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,13 @@ public class CompareNumberActivity extends AppCompatActivity {
         number1 = findViewById(R.id.number1);
         number2 = findViewById(R.id.number2);
 
+        Button btnToggleRule = findViewById(R.id.btnToggleRule);
+        btnToggleRule.setOnClickListener(v -> {
+            isGreaterThanRule = !isGreaterThanRule;
+            btnToggleRule.setText(isGreaterThanRule ? "Greater Than To Less Than" : "Less Than To Greater Than"); // 更新按钮文本
+            refreshNumbers();
+        });
+
         refreshNumbers();
 
         View.OnClickListener numberClickListener = view -> {
@@ -30,22 +38,22 @@ public class CompareNumberActivity extends AppCompatActivity {
             int num1 = Integer.parseInt(number1.getText().toString());
             int num2 = Integer.parseInt(number2.getText().toString());
 
-            // Check which number is bigger
-            if ((isNumber1Clicked && num1 > num2) || (!isNumber1Clicked && num2 > num1)) {
+            boolean condition = isGreaterThanRule ?
+                    (isNumber1Clicked && num1 > num2) || (!isNumber1Clicked && num2 > num1) :
+                    (isNumber1Clicked && num1 < num2) || (!isNumber1Clicked && num2 < num1);
+
+            if (condition) {
                 view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.pure_green));
             } else {
                 view.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.pure_red));
             }
 
-
-            // Wait a bit before refreshing numbers
             handler.postDelayed(this::refreshNumbers, 500);
         };
 
         number1.setOnClickListener(numberClickListener);
         number2.setOnClickListener(numberClickListener);
         findViewById(R.id.btnHome).setOnClickListener(v -> startActivity(new Intent(CompareNumberActivity.this, MainActivity.class)));
-
     }
 
     private void refreshNumbers() {
